55123744b581de317ef2d1550e790fadd63f1c55
Austin Walker <awalk89@gmail.com>
2018-05-19 11:17:28 -0400

    Get a nice looking about activity

---
+++ app/src/main/java/edu/neu/madcourse/austinwalker/AboutActivity.java
@@ -0,0 +1,11 @@
+package edu.neu.madcourse.austinwalker;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class AboutActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
--- app/src/main/java/edu/neu/madcourse/austinwalker/MainFragment.java
+++ app/src/main/java/edu/neu/madcourse/austinwalker/MainFragment.java
@@ -2,6 +2,7 @@ package edu.neu.madcourse.austinwalker;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -20,19 +21,8 @@ public class MainFragment extends Fragment {
         aboutButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-                builder.setTitle(R.string.about_label);
-                builder.setMessage(R.string.about_text);
-                builder.setCancelable(false);
-                builder.setPositiveButton(R.string.ok_label,
-                        new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                // Do nothing
-                            }
-                        });
-                mDialog = builder.show();
-
+                Intent intent = new Intent(getActivity(), AboutActivity.class);
+                getActivity().startActivity(intent);
             }
         });
 

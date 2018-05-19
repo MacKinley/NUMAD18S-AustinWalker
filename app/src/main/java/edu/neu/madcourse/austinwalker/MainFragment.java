9d7da7ee399ae29bd6368421d264c2965f9eaa5c
Austin Walker <awalker@ccs.neu.edu>
2018-05-19 00:37:49 -0400

    Initial commit

---
+++ app/src/main/java/edu/neu/madcourse/austinwalker/MainFragment.java
@@ -0,0 +1,21 @@
+package edu.neu.madcourse.austinwalker;
+
+import android.app.AlertDialog;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainFragment extends Fragment {
+    private AlertDialog mDialog;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        // Handle buttons
+
+        return rootView;
+    }
+}

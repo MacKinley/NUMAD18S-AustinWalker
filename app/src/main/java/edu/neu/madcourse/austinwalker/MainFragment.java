1670accb5f4569adb854d7f03d0064cc9878c086
Austin Walker <awalker@ccs.neu.edu>
2018-05-19 00:50:38 -0400

    Make the about button do something

--- app/src/main/java/edu/neu/madcourse/austinwalker/MainFragment.java
+++ app/src/main/java/edu/neu/madcourse/austinwalker/MainFragment.java
@@ -1,6 +1,7 @@
 package edu.neu.madcourse.austinwalker;
 
 import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -15,7 +16,35 @@ public class MainFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
         // Handle buttons
+        View aboutButton = rootView.findViewById(R.id.about_button);
+        aboutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                builder.setTitle(R.string.about_label);
+                builder.setMessage(R.string.about_text);
+                builder.setCancelable(false);
+                builder.setPositiveButton(R.string.ok_label,
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                // Do nothing
+                            }
+                        });
+                mDialog = builder.show();
+
+            }
+        });
 
         return rootView;
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        // Don't keep the about dialog up
+        if (mDialog != null)
+            mDialog.dismiss();
     }
 }

894169ab8c0286065d8b7ee7c51a1e6a09182673
Austin Walker <awalk89@gmail.com>
2018-05-19 11:39:55 -0400

    Add placeholder text for IMEI

--- app/src/main/java/edu/neu/madcourse/austinwalker/AboutActivity.java
+++ app/src/main/java/edu/neu/madcourse/austinwalker/AboutActivity.java
@@ -2,10 +2,15 @@ package edu.neu.madcourse.austinwalker;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        TextView imei = (TextView) findViewById(R.id.imei_text);
+        String imeiStr = imei.getText().toString();
+        imei.setText(imeiStr + "null");
     }
 }

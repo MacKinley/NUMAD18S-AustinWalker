b2bdfe7cced01fef2b7ad06d47b5836bc3c58cf7
Austin Walker <awalk89@gmail.com>
2018-05-19 20:49:28 -0400

    Show IMEI

--- app/src/main/java/edu/neu/madcourse/austinwalker/AboutActivity.java
+++ app/src/main/java/edu/neu/madcourse/austinwalker/AboutActivity.java
@@ -1,16 +1,42 @@
 package edu.neu.madcourse.austinwalker;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.telephony.TelephonyManager;
 import android.widget.TextView;
 
 public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+    }
 
+    protected void onResume() {
+        super.onResume();
         TextView imei = (TextView) findViewById(R.id.imei_text);
         String imeiStr = imei.getText().toString();
-        imei.setText(imeiStr + "null");
+        imei.setText(imeiStr + " " + getDeviceIMEI());
+    }
+
+    public String getDeviceIMEI() {
+        String identifier = null;
+        TelephonyManager tm = (TelephonyManager) this.getSystemService(Context.TELEPHONY_SERVICE);
+        if (null != tm) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: ask for permissions
+                return "null";
+            }
+            identifier = tm.getDeviceId();
+        }
+        if (null == identifier || 0 == identifier.length()) {
+            identifier = Settings.Secure.getString(this.getContentResolver(), Settings.Secure.ANDROID_ID);
+        }
+
+        return identifier;
     }
 }
--- app/src/main/java/edu/neu/madcourse/austinwalker/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/austinwalker/MainActivity.java
@@ -1,45 +1,19 @@
 package edu.neu.madcourse.austinwalker;
 
-import android.Manifest;
-import android.content.Context;
-import android.content.pm.PackageManager;
-import android.provider.Settings;
-import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.telephony.TelephonyManager;
 import android.util.Log;
-
-import java.util.logging.Logger;
 
 public class MainActivity extends AppCompatActivity {
 
+    final static String tag = "MainActivity";
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.d("onCreate", getDeviceIMEI());
+        Log.d(tag, "onCreate");
+
         setContentView(R.layout.activity_main);
     }
 
-    public String getDeviceIMEI() {
-        String deviceUniqueIdentifier = null;
-        TelephonyManager tm = (TelephonyManager) this.getSystemService(Context.TELEPHONY_SERVICE);
-        if (null != tm) {
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
-                return "blah";
-            }
-            deviceUniqueIdentifier = tm.getDeviceId();
-        }
-        if (null == deviceUniqueIdentifier || 0 == deviceUniqueIdentifier.length()) {
-            deviceUniqueIdentifier = Settings.Secure.getString(this.getContentResolver(), Settings.Secure.ANDROID_ID);
-        }
-        return deviceUniqueIdentifier;
-    }
 }

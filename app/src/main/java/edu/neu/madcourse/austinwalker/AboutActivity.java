44885efe90f5b573e25de85ea57815f406748f08
Austin Walker <awalk89@gmail.com>
2018-06-03 15:02:20 -0400

    Potentially a better imei approach

--- app/src/main/java/edu/neu/madcourse/austinwalker/AboutActivity.java
+++ app/src/main/java/edu/neu/madcourse/austinwalker/AboutActivity.java
@@ -4,13 +4,14 @@ import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.telephony.TelephonyManager;
 import android.widget.TextView;
 
 public class AboutActivity extends AppCompatActivity {
+
+    private static final int PERMISSION_READ_PHONE_STATE = 0;
 
     static String imeiStr = "null";
 
@@ -21,28 +22,42 @@ public class AboutActivity extends AppCompatActivity {
 
     protected void onResume() {
         super.onResume();
-        TextView imei = (TextView) findViewById(R.id.imei_text);
+        TextView imei = findViewById(R.id.imei_text);
+        String imeiLabel = getResources().getString(R.string.imei_text);
 
         // Get device IMEI or show null if not allowed
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(AboutActivity.this, new String[]{Manifest.permission.READ_PHONE_STATE}, 1);
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_PHONE_STATE)) {
+                imeiStr = "Need permission to access";
+
+            } else {
+                ActivityCompat.requestPermissions(AboutActivity.this, new String[]{Manifest.permission.READ_PHONE_STATE}, PERMISSION_READ_PHONE_STATE);
+            }
+
+        } else {
+            imeiStr = fetchIMEI();
         }
 
-        String imeiLabel = getResources().getString(R.string.imei_text);
         imei.setText(imeiLabel + " " + imeiStr);
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
         switch (requestCode) {
-            case 1: {
+            case PERMISSION_READ_PHONE_STATE: {
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    TelephonyManager tm = (TelephonyManager) this.getSystemService(Context.TELEPHONY_SERVICE);
-                    imeiStr = tm.getDeviceId();
+                    imeiStr = fetchIMEI();
+                } else {
+                    imeiStr = "Permission denied";
                 }
 
                 return;
             }
         }
+    }
+
+    private String fetchIMEI() {
+        TelephonyManager tm = (TelephonyManager) this.getSystemService(Context.TELEPHONY_SERVICE);
+        return tm.getDeviceId();
     }
 }

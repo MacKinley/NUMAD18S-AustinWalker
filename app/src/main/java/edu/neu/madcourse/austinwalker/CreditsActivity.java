a5becfdb9ebfdac4a761e5e3aa6cae23304b9b1b
Austin Walker <awalk89@gmail.com>
2018-06-03 19:54:12 -0400

    Add acknowledgements activity

---
+++ app/src/main/java/edu/neu/madcourse/austinwalker/CreditsActivity.java
@@ -0,0 +1,14 @@
+package edu.neu.madcourse.austinwalker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreditsActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_credits);
+    }
+}
--- app/src/main/java/edu/neu/madcourse/austinwalker/DictionaryActivity.java
+++ app/src/main/java/edu/neu/madcourse/austinwalker/DictionaryActivity.java
@@ -1,12 +1,26 @@
 package edu.neu.madcourse.austinwalker;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
+import android.view.View;
 
 public class DictionaryActivity extends AppCompatActivity {
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dictionary);
+
+        // Acknowledgements button
+        View ackButton = findViewById(R.id.ack_button);
+        ackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DictionaryActivity.this, CreditsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }

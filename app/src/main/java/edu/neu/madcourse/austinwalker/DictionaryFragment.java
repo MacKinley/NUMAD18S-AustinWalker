65f8848824443f2d9c82e6c89405424cb24133ed
Austin Walker <awalk89@gmail.com>
2018-06-03 20:32:07 -0400

    Make sure to include 3 letter words

--- app/src/main/java/edu/neu/madcourse/austinwalker/DictionaryFragment.java
+++ app/src/main/java/edu/neu/madcourse/austinwalker/DictionaryFragment.java
@@ -43,7 +43,7 @@ public class DictionaryFragment extends Fragment {
             public void onTextChanged(CharSequence s, int start, int before, int count) {
                 String text = s.toString();
 
-                if (count > 3 && checkDictionaryWord(text)) {
+                if (count >= 3 && checkDictionaryWord(text)) {
                     wordList.add(0, text);
                     beep();
                     printWordList();
